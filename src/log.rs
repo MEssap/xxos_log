7dcbf93755c9121b5fd739d2b52795e5c2df4a8d
wangrui <wangrui@baihai.ai>
2024-01-11 10:45:05 +0800

    support macro for log

---
+++ src/log.rs
@@ -0,0 +1,56 @@
+use core::fmt;
+
+pub trait WriteLog: Sync {
+    fn print(&self, log_content: core::fmt::Arguments);
+}
+
+pub struct Log<'a> {
+    pub writer: &'a dyn WriteLog,
+}
+
+impl<'a> Log<'a> {
+    pub fn init(writer: &'a dyn WriteLog) -> Self {
+        Self { writer }
+    }
+
+    #[track_caller]
+    pub fn info(&self, s: fmt::Arguments) {
+        let location = core::panic::Location::caller();
+
+        let file_name = location.file();
+        let file_line = location.line();
+
+        let logid = "logid";
+
+        self.writer.print(format_args!(
+            "{}\t[\x1b[32mINFO]: {}:{} - {}\x1b[0m",
+            logid, file_name, file_line, s
+        ));
+    }
+
+    #[track_caller]
+    pub fn warnning(&self, s: fmt::Arguments) {
+        let location = core::panic::Location::caller();
+        let logid = "logid";
+        self.writer.print(format_args!(
+            "{}\t[\x1b[33mWARN]: {}:{} - {}\x1b[0m",
+            logid,
+            location.file(),
+            location.line(),
+            s
+        ));
+    }
+
+    #[track_caller]
+    pub fn error(&self, s: fmt::Arguments) {
+        let location = core::panic::Location::caller();
+        let logid = "logid";
+        self.writer.print(format_args!(
+            "{}\t[\x1b[31mERROR]: {}:{} - {}\x1b[0m",
+            logid,
+            location.file(),
+            location.line(),
+            s
+        ));
+    }
+}
